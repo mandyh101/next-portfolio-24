d651a027bf80247e08afb019decf2f49ae499b92
mandy <mandyhale10@gmail.com>
2024-08-29 15:57:19 +1200

    WIP - add nav top style later when more page content

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
 import HeroWave from "@/components/Hero-wave";
 import HeroSpotlight from "@/components/Hero-spotlight";
+import { FloatingNav } from "@/components/ui/FloatingNav";
 
 export default function Home() {
+
+  const navItems = [
+    { name: "Me", link: "#me"},
+    { name: "Projects", link: "#projects" },
+    { name: "Skills", link: "#skills" },
+    { name: "Contact", link: "#contact" },
+  ]
+  
   return (
     <main className="relative bg-seasalt-white flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+        <FloatingNav  navItems={navItems}/>
         <HeroSpotlight />
         {/* <HeroWave /> */}
       </div>
