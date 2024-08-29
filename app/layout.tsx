91f91614497f7eab11fcc1e5222a0f9d75e47551
mandy <mandyhale10@gmail.com>
2024-08-29 14:28:34 +1200

    remove set weight for more flexibility

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,13 +4,13 @@ import "./globals.css";
 
 const raleway = Raleway({
    subsets: ["latin"],
-   weight: '300',
+  //  weight: '300',
    variable: '--font-raleway', // Defining a CSS variable for the font
   });
 
 const robotoSlab = Roboto_Slab({
   subsets: ["latin"],
-  weight: '400',
+  // weight: '400',
   variable: '--font-roboto-slab'
   });
 
