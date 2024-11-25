921a967b85805d620e865f7dbd28163ea24e0b47
mandy <mandyhale10@gmail.com>
2024-11-26 08:29:59 +1300

    style: interests box item

--- components/AboutMeGridItems/Interests.tsx
+++ components/AboutMeGridItems/Interests.tsx
@@ -1,12 +1,24 @@
+import Image from 'next/image'
 import React from 'react'
-
 
 const Interests = () => {
   return (
-    <div className="bentoGridItem group">
-      <div className='bentoGridItemInner'>
-        <h3 className='text-neutral-600 dark:text-neutral-200 mb-2 mt-2'>Interests</h3>
-        <p className='font-normal text-neutral-600 text-xs dark:text-neutral-300'>Surfing etc etc...</p>
+    <div className="bentoGridItem group overflow-hidden relative">
+      <Image
+        src="/images/mandysurfs.jpg"
+        alt=""
+        className="object-cover transition duration-200 group-hover:scale-110"
+        fill
+      />
+      <div className="absolute inset-0 bg-black/30"></div>
+      <div className="relative z-10">
+        <h3 className="sr-only">Interests</h3>
+        <p className="font-normal text-white">
+          Ocean lover, mountain biker, trail runner. When I&apos;'m not
+          adventuring outside, I am an active member of Climate Action Tech,
+          Women in Tech Christchurch and a volunteer Surf Life Guard at Taylors
+          Mistake Surf Life Saving Club.{' '}
+        </p>
       </div>
     </div>
   )
